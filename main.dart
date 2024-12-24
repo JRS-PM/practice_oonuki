fb21e46b48f99d0dc5ee118e7803d15571475905
KoyaOnuki <onuki@alljrs.co.jp>
2024-12-24 09:41:33 +0900

    maindart

--- main.dart
+++ main.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
 
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
-//プルリクテスト
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -42,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String title = "回";
 
   void _incrementCounter() {
     setState(() {
@@ -51,30 +41,112 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    (
+      _counter,
+      style: const TextStyle(
+        fontSize: 150,
+        fontWeight: FontWeight.bold,
+        color: Color.fromARGB(255, 176, 56, 23),
+      ),
+    );
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 199, 203, 136),
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: const Color.fromARGB(255, 251, 227, 105),
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        children: <Widget>[
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 240, 238, 174),
+              borderRadius: BorderRadius.circular(20),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Center(
+              child: Text(
+                '$_counter回',
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 8, 8, 8),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20), // 間隔を追加
+          Wrap(
+            spacing: 20.0,
+            runSpacing: 15.0,
+            children: List.generate(_counter, (index) {
+              return Container(
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    border: Border.all(color: Colors.black)),
+                width: 70,
+                height: 70,
+                alignment: Alignment.center,
+                child: Text('$index'),
+              );
+            }),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
+
+class TEST extends _MyHomePageState {
+  @override
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Text("$_counter");
+  }
+}
+
+//       body: Container(
+//         color: Colors.blue,
+//           decoration: BoxDecoration(
+//             // color: const Color.fromARGB(255, 163, 243, 33),
+//             // shape: BoxShape.circle,
+//             borderRadius: BorderRadius.circular(20),
+//           ),
+//           child: Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 const Text(
+//                   'You have pushed the button this many times:',
+//                 ),
+//                 Text(
+//                   '$_counter$title',
+//                   style: const TextStyle(
+//                     backgroundColor: Colors.yellow,
+//                     fontSize: 50,
+//                     fontWeight: FontWeight.bold,
+//                     color: Colors.blue,
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
