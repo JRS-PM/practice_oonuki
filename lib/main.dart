07460b3f4819608a6997a4ad2e108b923159a381
KoyaOnuki <onuki@alljrs.co.jp>
2024-12-27 12:11:47 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'widget.dart';
+import 'package:flutter_application_practice3/widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
