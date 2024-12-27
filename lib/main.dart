cc6d99b261e27374d43ae0e35c546e2b179f46fa
KoyaOnuki <onuki@alljrs.co.jp>
2024-12-27 14:14:31 +0900

    ファイル整理

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_practice3/widget.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
