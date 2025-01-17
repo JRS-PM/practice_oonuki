4aa171882b345479ecceed91fcfb6ddc49e73645
KoyaOnuki <onuki@alljrs.co.jp>
2025-01-17 14:05:28 +0900

    Practice4

--- lib/practice4/article.dart
+++ lib/practice4/article.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'article.freezed.dart';
 part 'article.g.dart';
@@ -13,4 +14,27 @@ class Article with _$Article {
 
   factory Article.fromJson(Map<String, dynamic> json) =>
       _$ArticleFromJson(json);
+}
+
+@riverpod
+class ArticleListNotifier extends AutoDisposeNotifier<List<Article>> {
+  @override
+  List<Article> build() {
+    return [];
+  }
+
+  void addArticle(Article article) {
+    state = [...state, article];
+  }
+
+  void removeArticle(int code) {
+    state = state.where((article) => article.code != code).toList();
+  }
+
+  void updateArticle(Article updatedArticle) {
+    state = state
+        .map((article) =>
+            article.code == updatedArticle.code ? updatedArticle : article)
+        .toList();
+  }
 }
